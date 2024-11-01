d610751a8d73bbb289c7b66c76b0e2439a0cb858
Phijose J Philip <phijosewiz@gmail.com>
2024-11-01 16:38:12 +0530

    downgraded scala, added resource data, Load and describe is done

--- build.sbt
+++ build.sbt
@@ -1,9 +1,16 @@
 ThisBuild / version := "0.1.0-SNAPSHOT"
 
-ThisBuild / scalaVersion := "3.3.4"
+ThisBuild / scalaVersion := "2.13.14"
 
 lazy val root = (project in file("."))
   .settings(
     name := "LACrimeDataAnalysis",
     idePackagePrefix := Some("com.tigmaminds.lacrimedataanalysis")
   )
+
+val sparkVersion = "3.5.3"
+
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % sparkVersion,
+  "org.apache.spark" %% "spark-sql" % sparkVersion,
+)
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,6 +1,18 @@
 package com.tigmaminds.lacrimedataanalysis
 
-@main
-def main(): Unit = {
-  println("Hello world!")
+import org.apache.spark.sql.SparkSession
+
+object Main{
+  def main(stringArgs: Array[String]): Unit = {
+    val spark = SparkSession.builder()
+      .appName("LA Crime Data Analysis")
+      .master("local")
+      .getOrCreate()
+
+    val LACdf = spark.read
+      .option("header",true)
+      .csv("./src/main/resources/LACrimeData/Crime_Data_from_2020_to_Present.csv")
+
+    LACdf.describe().show()
+  }
 }
