0fff34a488ce4a609e4afca0b0a969cc6df5e292
Phijose J Philip <phijosewiz@gmail.com>
2024-11-01 16:56:35 +0530

    basic CrimeCommited Table created

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,6 +1,7 @@
 package com.tigmaminds.lacrimedataanalysis
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
 
 object Main{
   def main(stringArgs: Array[String]): Unit = {
@@ -9,10 +10,20 @@ object Main{
       .master("local")
       .getOrCreate()
 
+    val startingIndex = 1
+
     val LACdf = spark.read
-      .option("header",true)
+      .option("header",value = true)
       .csv("./src/main/resources/LACrimeData/Crime_Data_from_2020_to_Present.csv")
 
-    LACdf.describe().show()
+    LACdf.printSchema()
+
+    val CrimeCommited = LACdf
+      .select("Crm Cd 1","Crm Cd 2","Crm Cd 3","Crm Cd 4")
+      .withColumn("CrimesId",monotonically_increasing_id() + startingIndex)
+
+//    CrimeCommited.write.
+
+    CrimeCommited.show()
   }
 }
